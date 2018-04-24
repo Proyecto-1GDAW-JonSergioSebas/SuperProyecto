7075bc495a5c70a1f9422c0fa23dfb8e7e41e196
Unknown <jon.maneiro@ikasle.egibide.org>
2018-04-24 09:04:20 +0200

    Starting the calendar

--- SuperProyecto/src/DB/DBController.java
+++ SuperProyecto/src/DB/DBController.java
@@ -5,6 +5,10 @@
  */
 package DB;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * @author Sebastián Zawisza
  * @author Sergio Zulueta
@@ -13,5 +17,27 @@ package DB;
  * @since 1.0
  */
 public class DBController {
+    /**
+     * Este metodo crea la conexión a la base de datos
+     * @throws ClassNotFoundException no encuentra la clase
+     * @throws SQLException hay una excepcion SQL
+     * @return la conexion a la base de datos
+     */
+    public static Connection createConnection() throws ClassNotFoundException, SQLException{
+        
+        Class.forName("oracle.jdbc.OracleDriver");//Indicar el driver
+        Connection con= DriverManager.getConnection("jdbc:oracle:thin:@SrvOracle:1521:orcl","eqdaw01","eqdaw01");//Crear la conexion
+        
+        return con;
+    }
     
+    /**
+     * Pide a la clase DBTeam el numero de equipos que hay.
+     * 
+     * @return el numero de equipos
+     * @see DBTeam.getTeamQuantity()
+     */
+    public static int teamQuantity(){
+        
+    }
 }
